86368fa806e58618eeb0701a556242e4c6803597
pxyvlrd <priv.hubertjucha@gmail.com>
2025-07-07 00:18:48 +0200

    Building Districts

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ let quarries = 0;
 const sawmillsDisplay = document.getElementById('sawmills');
 const quarriesDisplay = document.getElementById('quarries');
 
+//districts
+let scienceDistrict = 0;
+let cultureDistrict = 0;
+let religionDistrict = 0;
+
 //buttons
 const foodButtonClicker = document.getElementById('gatherFood');
 const woodButtonClicker = document.getElementById('gatherWood');
@@ -62,11 +67,13 @@ function renderBuilders() {
         builderInfo.textContent = builderText;
 
         const buildSawmillBtn = document.createElement('button');
-        buildSawmillBtn.textContent = 'Build Sawmill';
+        buildSawmillBtn.textContent = 'Build Sawmill (100 wood & 100 stone)';
         buildSawmillBtn.addEventListener('click', () => {
-            if (builder.builderPoints > 0) {
+            if (builder.builderPoints > 0 && wood >= 10 && stone >= 10) {
                 builder.builderPoints--;
                 sawmills++;
+                wood = wood - 10;
+                stone = stone - 10;
 
                 if (builder.builderPoints === 0) {
                     setTimeout(() => {
@@ -80,11 +87,13 @@ function renderBuilders() {
         });
 
         const buildQuarryBtn = document.createElement('button');
-        buildQuarryBtn.textContent = 'Build Quarry';
+        buildQuarryBtn.textContent = 'Build Quarry (100 wood & 100 stone)';
         buildQuarryBtn.addEventListener('click', () => {
-            if (builder.builderPoints > 0) {
+            if (builder.builderPoints > 0 && wood >= 10 && stone >= 10) {
                 builder.builderPoints--;
                 quarries++;
+                wood = wood - 10;
+                stone = stone - 10;
 
                 if (builder.builderPoints === 0) {
                     setTimeout(() => {
@@ -101,8 +110,88 @@ function renderBuilders() {
         builderContainer.appendChild(buildSawmillBtn);
         builderContainer.appendChild(buildQuarryBtn);
 
+        const buildScienceDistrictBtn = document.createElement('button');
+        buildScienceDistrictBtn.textContent = 'Build Science District (500 wood & 500 stone & 3 builder points)';
+
+        if (scienceDistrict === 0) {
+            buildScienceDistrictBtn.addEventListener('click', () => {
+                if (builder.builderPoints >= 3 && wood >= 50 && stone >= 50) {
+                    builder.builderPoints = builder.builderPoints - 3;
+                    scienceDistrict++;
+                    wood = wood - 50;
+                    stone = stone -50;
+
+                    if (builder.builderPoints === 0) {
+                        setTimeout(() => {
+                        builders.splice(index, 1);
+                        updateUI();
+                    }, 50);
+                    } else {
+                    updateUI();
+                    }
+                }
+            });
+            
+            builderContainer.appendChild(buildScienceDistrictBtn);
+        }
+
+        const buildCultureDistrictBtn = document.createElement('button');
+        buildCultureDistrictBtn.textContent = 'Build Culture District (500 wood & 500 stone & 3 builder points)';
+
+        if (cultureDistrict === 0) {
+            buildCultureDistrictBtn.addEventListener('click', () => {
+                if (builder.builderPoints >= 3 && wood >= 50 && stone >= 50) {
+                    builder.builderPoints = builder.builderPoints - 3;
+                    cultureDistrict++;
+                    wood = wood - 50;
+                    stone = stone -50;
+
+                    if (builder.builderPoints === 0) {
+                        setTimeout(() => {
+                        builders.splice(index, 1);
+                        updateUI();
+                    }, 50);
+                    } else {
+                    updateUI();
+                    }
+                }
+            });
+            
+            builderContainer.appendChild(buildCultureDistrictBtn);
+        }
+
+        const buildReligionDistrictBtn = document.createElement('button');
+        buildReligionDistrictBtn.textContent = 'Build Religion District (500 wood & 500 stone & 3 builder points)';
+
+        if (religionDistrict === 0) {
+            buildReligionDistrictBtn.addEventListener('click', () => {
+                if (builder.builderPoints >= 3 && wood >= 50 && stone >= 50) {
+                    builder.builderPoints = builder.builderPoints - 3;
+                    religionDistrict++;
+                    wood = wood - 50;
+                    stone = stone -50;
+
+                    if (builder.builderPoints === 0) {
+                        setTimeout(() => {
+                        builders.splice(index, 1);
+                        updateUI();
+                    }, 50);
+                    } else {
+                    updateUI();
+                    }
+                }
+            });
+            
+            builderContainer.appendChild(buildReligionDistrictBtn);
+        }
+
+
         buildersList.appendChild(builderContainer);
     });
+}
+
+function districtsSection() {
+    
 }
 
 function materialsClicker() {
@@ -128,9 +217,12 @@ function materialsClicker() {
 
 }
 
-function buildingsProduction() {
+function Production() {
     wood = wood + sawmills * 1;
     stone = stone + quarries * 1;
+    sciencePoints = sciencePoints + scienceDistrict * 1;
+    culturePoints = culturePoints + cultureDistrict * 1;
+    religionPoints = religionPoints + religionDistrict * 1;
     updateUI();
 }
 
@@ -154,7 +246,7 @@ function updateUI() {
 function Game() {
     materialsClicker();
     buyBuilderBtn.addEventListener('click', buyBuilder);
-    setInterval(buildingsProduction, 1000);
+    setInterval(Production, 1000);
 
 }
 
